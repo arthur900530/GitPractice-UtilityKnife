6e825c8ffe783b9402d1d0f8739501be6cd2a4e3
Arthur Chien <moneychien20639@gmail.com>
2023-06-23 15:30:37 +0800

    add methods multiply and division

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -5,5 +5,8 @@ class Calculator():
         return x + y
     def subtract(x, y):
         return x - y
-    
+    def multiply(x, y):
+        return x * y
+    def division(x, y):
+        return x / y
     
